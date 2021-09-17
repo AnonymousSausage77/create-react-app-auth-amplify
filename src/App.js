853b5795ed7b80bb060415608860f012c74cf53b
AnonymousSausage77 <johnbures@hotmail.com>
2021-09-17 16:44:24 +0930

    testing

--- src/App.js
+++ src/App.js
@@ -9,36 +9,14 @@ Amplify.configure(aws_exports);
 import { Jumbotron, Button, Container, Col } from 'reactstrap';
 import {BrowserRouter as Router, Route, Link, Switch } from 'react-router-dom';
 
-import AppNavbar from './Navbar.js';
 import AppJumbotron from './AppJumbotron.js';
-import AppSignin from './AppSignin.js';
-import DriverLanding from './DriverLanding.js';
-import CustomerLanding from './CustomerLanding.js';
-import AdminLanding from './AdminLanding.js';
 
 class App extends Component {
   render() {
     return (
       <div className="App pb-4 html" >
         <AmplifySignOut />
-        <Router>
-          <Switch>
-            <Route exact path="/" component={AppJumbotron}/>
-            <Route path="/sign-in" component={AppSignin} />
-            <Route path="/driver" >
-              <AppNavbar/>
-              <DriverLanding />
-            </Route>
-            <Route path="/user" >
-              <AppNavbar/>
-              <CustomerLanding/>
-            </Route>
-            <Route path="/admin" >
-              <AppNavbar/>
-              <AdminLanding/>
-            </Route>
-          </Switch>
-        </Router>
+        <Route exact path="/" component={AppJumbotron}/>
       </div>
     );
   }
