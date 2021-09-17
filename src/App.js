f96c447a9a0398b03e8a6f16225e2218f300604a
AnonymousSausage77 <johnbures@hotmail.com>
2021-09-17 17:33:12 +0930

    Adds landing pages and nav bars

--- src/App.js
+++ src/App.js
@@ -2,6 +2,11 @@ import React, { Component } from 'react';
 import logo from './logo.svg';
 
 import AppJumbotron from './AppJumbotron.js';
+import AppNavbar from './Navbar.js';
+import DriverLanding from './DriverLanding.js';
+import CustomerLanding from './CustomerLanding.js';
+import AdminLanding from './AdminLanding.js';
+
 import {BrowserRouter as Router, Route, Link, Switch } from 'react-router-dom';
 
 import './App.css';
@@ -15,12 +20,24 @@ class App extends Component {
   render() {
     return (
       <div className="App">
-        <AmplifySignOut />
-        <Switch>
-          <Route exact path="/">
-            <AppJumbotron />
-          </Route>
-        </Switch>
+        <AppNavbar />
+        <Router>
+          <Switch>
+            <Route exact path="/" component={AppJumbotron}/>
+            <Route path="/driver" >
+              <AppNavbar/>
+              <DriverLanding />
+            </Route>
+            <Route path="/user" >
+              <AppNavbar/>
+              <CustomerLanding/>
+            </Route>
+            <Route path="/admin" >
+              <AppNavbar/>
+              <AdminLanding/>
+            </Route>
+          </Switch>
+        </Route>
       </div>
     );
   }
