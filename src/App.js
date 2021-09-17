3d5ec88c28e555edc5f53ca31ddda501014ae596
AnonymousSausage77 <johnbures@hotmail.com>
2021-09-17 16:56:44 +0930

    test reset

--- src/App.js
+++ src/App.js
@@ -6,17 +6,25 @@ import Amplify from 'aws-amplify';
 import aws_exports from './aws-exports';
 Amplify.configure(aws_exports);
 
-import { Jumbotron, Button, Container, Col } from 'reactstrap';
-import {BrowserRouter as Router, Route, Link, Switch } from 'react-router-dom';
-
-import AppJumbotron from './AppJumbotron.js';
-
 class App extends Component {
   render() {
     return (
-      <div className="App pb-4 html" >
+      <div className="App">
         <AmplifySignOut />
-        <AppJumbotron />
+        <header className="App-header">
+          <img src={logo} className="App-logo" alt="logo" />
+          <p>
+            Edit <code>src/App.js</code> and save to reload.
+          </p>
+          <a
+            className="App-link"
+            href="https://reactjs.org"
+            target="_blank"
+            rel="noopener noreferrer"
+          >
+            Learn React
+          </a>
+        </header>
       </div>
     );
   }
