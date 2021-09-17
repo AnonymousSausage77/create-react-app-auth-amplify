165c2ba38333901cffd8a9aa3d6a37c22704e0e3
AnonymousSausage77 <johnbures@hotmail.com>
2021-09-17 17:11:46 +0930

    fix 3

--- src/App.js
+++ src/App.js
@@ -1,12 +1,14 @@
 import React, { Component } from 'react';
 import logo from './logo.svg';
+
+import AppJumbotron from './AppJumbotron.js';
+
 import './App.css';
 import { withAuthenticator, AmplifySignOut } from '@aws-amplify/ui-react'
 import Amplify from 'aws-amplify';
 import aws_exports from './aws-exports';
 Amplify.configure(aws_exports);
 
-import AppJumbotron from './AppJumbotron.js';
 
 class App extends Component {
   render() {
--- src/AppJumbotron.js
+++ src/AppJumbotron.js
@@ -1,6 +1,5 @@
 import React from 'react';
 import { Jumbotron, Button, Container, Col } from 'reactstrap';
-import { Link } from 'react-router-dom'
 
 var AppJumbotron = (props) => {
   return (<><Jumbotron className="m-2 pt-5 my-5">
